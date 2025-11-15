e5d1da00886f1772946de3cd66d48f84b7f800e4
chaeyi0318 <chaeyi0318@gmail.com>
2025-11-16 01:02:38 +0900

    feat: 연관관계 설정

--- BE/src/main/java/com/project/couponing/coupon/entity/CouponIssue.java
+++ BE/src/main/java/com/project/couponing/coupon/entity/CouponIssue.java
@@ -2,28 +2,37 @@ package com.project.couponing.coupon.entity;
 
 import com.project.couponing.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"user_id", "event_id"}),
-        @UniqueConstraint(columnNames = {"request_id"})
+        @UniqueConstraint(
+                name = "uk_issue_user_event",
+                columnNames = {"user_id", "event_id"}),
+        @UniqueConstraint(
+                name = "uk_issue_request_id",
+                columnNames = {"request_id"})
     }
 )       // 한 유저가 같은 이벤트에서 쿠폰을 한 번만 받을 수 있도록 제약
+@Getter
+@NoArgsConstructor
 public class CouponIssue {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String requestId;       // idempotency 보장용
+    private String requestId;       // idempotency 보장용 (중복 요청 구분용)
 
     private LocalDateTime createdAt;        // 발급 시간
 
     @ManyToOne
-    private User user;
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;      // 누가
 
     @ManyToOne
-    private CouponEvent event;
-
+    @JoinColumn(name = "event_id", nullable = false)
+    private CouponEvent event;      // 어떤 이벤트에서
 }
