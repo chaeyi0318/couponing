fd759c7424a2d80602d7124474f21fb82e0c9482
chaeyi0318 <chaeyi0318@gmail.com>
2025-11-19 20:02:16 +0900

    fix: startAt, endAt 수정

--- BE/src/main/java/com/project/couponing/coupon/entity/CouponEvent.java
+++ BE/src/main/java/com/project/couponing/coupon/entity/CouponEvent.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class CouponEvent {
     @Id
@@ -14,4 +16,8 @@ public class CouponEvent {
     private String name;
 
     private int totalStock;     // 총 재고
+
+    private LocalDateTime startAt;
+
+    private LocalDateTime endAt;
 }
