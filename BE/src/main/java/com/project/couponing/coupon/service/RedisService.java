521cea04d3f086c27693cc45e8b0a5e88f81c866
chaeyi0318 <chaeyi0318@gmail.com>
2025-11-21 18:32:25 +0900

    feat: redis 재고 관리 구현

---
+++ BE/src/main/java/com/project/couponing/coupon/service/RedisService.java
@@ -0,0 +1,30 @@
+package com.project.couponing.coupon.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final StringRedisTemplate redisTemplate;
+
+    //  redis 키
+    private String stockKey (Long eventId) {
+        return "stock:event:" + eventId;
+    }
+
+    // 재고조회
+    public int getStock(Long eventId) {
+        String value = redisTemplate.opsForValue().get(stockKey(eventId));
+
+        return value != null ? Integer.parseInt(value) : 0;
+    }
+
+    // 재고차감
+    public boolean decreaseStock(Long eventId) {
+        Long result = redisTemplate.opsForValue().decrement(stockKey(eventId));
+        return result != null && result >= 0;
+    }
+}
