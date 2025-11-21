83e99a9f49900eb3247dc5636a8d7985ab71d93e
chaeyi0318 <chaeyi0318@gmail.com>
2025-11-21 20:02:00 +0900

    feat: 쿠폰 발급 후 재고감소

--- BE/src/main/java/com/project/couponing/coupon/service/RedisService.java
+++ BE/src/main/java/com/project/couponing/coupon/service/RedisService.java
@@ -1,7 +1,6 @@
 package com.project.couponing.coupon.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +24,8 @@ public class RedisService {
     // 재고차감
     public boolean decreaseStock(Long eventId) {
         Long result = redisTemplate.opsForValue().decrement(stockKey(eventId));
+
+//        System.out.println("DECR " + stockKey(eventId) + " -> " + result);
         return result != null && result >= 0;
     }
 }
