404afa713ef817be1f16aec66846bde7f24f07e0
chaeyi0318 <chaeyi0318@gmail.com>
2025-11-23 15:19:01 +0900

    feat: 재고 조회 컨트롤러

--- BE/src/main/java/com/project/couponing/coupon/controller/CouponIssueController.java
+++ BE/src/main/java/com/project/couponing/coupon/controller/CouponIssueController.java
@@ -2,6 +2,7 @@ package com.project.couponing.coupon.controller;
 
 import com.project.couponing.common.ApiSuccess;
 import com.project.couponing.coupon.service.CouponIssueService;
+import com.project.couponing.coupon.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CouponIssueController {
     private final CouponIssueService couponIssueService;
+    private final RedisService redisService;
 
+    // 쿠폰 발급
     @PostMapping("/{eventId}/coupon")
     public ResponseEntity<ApiSuccess> issueCoupon(@PathVariable Long eventId,
                                                   @RequestHeader("X-User-Id") Long userId,
                                                   @RequestHeader("X-Request-Id") String requestId) {
         return couponIssueService.issueCoupon(eventId, userId, requestId);
+    }
+
+    // 재고 조회
+    @GetMapping("/{eventId}/stock")
+    public int getStock(@PathVariable Long eventId) {
+        return redisService.getStock(eventId);
     }
 }
